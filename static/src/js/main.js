bc137d21967877a704d173ea41610ebc047f3fd3
3lvis-dev <ea.espinoza.f@gmail.com>
2024-09-10 17:22:25 -0400

    v1.1 Optimization code

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,9 +1,13 @@
+// Mostrar el nombre del proyecto en la consola
 let project = "Start project Seven Suites";
 console.log(project);
 
+// Seleccionar el ícono de la hamburguesa y el menú de navegación
 const iconBurger = document.querySelector("#icon_burger");
 const menu = document.querySelector("#menu");
 
+// Añadir un evento de clic al ícono de la hamburguesa
 iconBurger.addEventListener("click", () => {
+    // Alternar la clase 'show_menu' en el menú de navegación para mostrar u ocultar
     menu.classList.toggle("show_menu");
 });
